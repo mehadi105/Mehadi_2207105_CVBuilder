241f18eccb9cd239d5afb0e80ccf0b41a2acaf1b
mehadi105 <147333420+mehadi105@users.noreply.github.com>
2025-11-18 03:15:08 +0600

    Hellocontroller implementation

--- src/main/java/com/example/practicefx/HelloController.java
+++ src/main/java/com/example/practicefx/HelloController.java
@@ -1,14 +1,30 @@
 package com.example.practicefx;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private void onCreateNewCV(ActionEvent event) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("create-view.fxml"));
+            Scene scene = new Scene(fxmlLoader.load());
+
+            Stage currentStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            currentStage.setTitle("CV Builder - Creation Form");
+            currentStage.setScene(scene);
+            currentStage.show();
+
+        } catch (IOException e) {
+            System.err.println("Error loading create-view.fxml:");
+            e.printStackTrace();
+        }
     }
 }
